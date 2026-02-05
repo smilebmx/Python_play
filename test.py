c0e43f5bc60a01d0150985beede6f6908d01ae77
Rostyslav Pereprosov <pereprosoff@gmail.com>
2026-02-04 22:47:46 -0800

    fixe

--- test.py
+++ test.py
@@ -111,7 +111,6 @@ print(f"File saved (replaced if existed): {path}")
 # transformations...
 logging.info("Output file created")
 print("new Branch test")
-print("new Branch test")
 #print(sum_sales)  
 #print(sum_sales_group)  
 #print(monthly_sales)
